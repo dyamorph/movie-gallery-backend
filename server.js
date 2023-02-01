0f023366c6a8c5b5eb1a5ff710feaf0d2d4c7662
dyamorph <dyamorph22@gmail.com>
2023-02-01 14:45:24 +0300

    fix content-security

--- server.js
+++ server.js
@@ -11,7 +11,10 @@ app.use(cors());
 app.use(express.json());
 app.use(movieRoutes);
 app.use(userRoutes);
-
+app.use(function(req, res, next) {
+res.setHeader("content-security-policy-report-only", "default-src 'self'; script-src 'self' 'report-sample'; style-src 'self' 'report-sample'; base-uri 'none'; object-src 'none'; report-uri https://5e52f4c893efcda6a7d40460.endpoint.csper.io")
+next();
+});
 mongoose
   .connect(URL)
   .then(() => console.log('Connected to MongoDB'))
