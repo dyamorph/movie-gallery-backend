34393559943c5dfc7231694a29a56fd299cbe86b
dyamorph <dyamorph22@gmail.com>
2023-02-01 23:03:18 +0300

    fix cors

--- server.js
+++ server.js
@@ -12,9 +12,10 @@ app.use(express.json());
 app.use(movieRoutes);
 app.use(userRoutes);
 app.use(function(req, res, next) {
-res.setHeader('Content-Security-Policy-Report-Only', "default-src 'self'; script-src 'self';" +
-  " style-src 'self'; font-src 'self'; img-src 'self'; frame-src 'self'")
-next();
+  res.setHeader('Content-Security-Policy-Report-Only', "default-src 'self'; script-src 'self';" +
+  " style-src 'self'; font-src 'self'; img-src 'self'; frame-src 'self'");
+  res.setHeader('Access-Control-Allow-Origin', 'true');
+  next();
 });
 mongoose
   .connect(URL)
